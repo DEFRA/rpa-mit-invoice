b80f494b6ac84b4826634ff74d380224124072cc
steve-dickinson <steve.dickinson@defra.gov.uk>
2023-03-09 08:16:27 +0000

    Add additional tests for aprroval

--- EST.MIT.Invoice.Api.Test/InvoicePutEndpointsTests.cs
+++ EST.MIT.Invoice.Api.Test/InvoicePutEndpointsTests.cs
@@ -28,7 +28,7 @@ public class InvoicePutEndpointTests
         {
             Id = invoiceId,
             Scheme = scheme,
-            Status = "Awaiting",
+            Status = "awaiting",
             CreatedBy = "test",
             UpdatedBy = "test",
             Header = new InvoiceHeader
@@ -49,6 +49,8 @@ public class InvoicePutEndpointTests
 
         result.GetOkObjectResultStatusCode().Should().Be(200);
         result.GetOkObjectResultValue<Invoice>().Should().BeEquivalentTo(invoice);
+
+        await _queueService.DidNotReceive().CreateMessage("");
     }
 
     [Fact]
@@ -81,6 +83,44 @@ public class InvoicePutEndpointTests
         var result = await InvoiceEndpoints.UpdateInvoice(invoice, _tableService, _queueService, _validator);
 
         result.GetBadRequestStatusCode().Should().Be(400);
+
+        await _queueService.DidNotReceive().CreateMessage("");
+    }
+
+    [Fact]
+    public async Task PutInvoicebySchemeAndInvoiceId_WhenApproved()
+    {
+        const string scheme = "bps";
+        const string invoiceId = "123456789";
+
+        var invoice = new Invoice
+        {
+            Id = invoiceId,
+            Scheme = scheme,
+            Status = "approved",
+            CreatedBy = "test",
+            UpdatedBy = "test",
+            Header = new InvoiceHeader
+            {
+                Id = "123456789",
+                ClaimReference = "123456789",
+                ClaimReferenceNumber = "123456789",
+                FRN = "123456789",
+                AgreementNumber = "123456789",
+                Currency = "GBP",
+                Description = "Test"
+            }
+        };
+
+        _tableService.UpdateInvoice(invoice).Returns(true);
+
+        var result = await InvoiceEndpoints.UpdateInvoice(invoice, _tableService, _queueService, _validator);
+
+        result.GetOkObjectResultStatusCode().Should().Be(200);
+        result.GetOkObjectResultValue<Invoice>().Should().BeEquivalentTo(invoice);
+
+        var expectedMessage = $"{{\"id\":\"{invoiceId}\",\"scheme\":\"{scheme}\"}}";
+        await _queueService.Received().CreateMessage(expectedMessage);
     }
 
     [Theory]
