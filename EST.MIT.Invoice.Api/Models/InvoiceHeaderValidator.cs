2b7a115c33cb1bed43f21dd720215503e3018d69
Henry Adetunji <Henry.Adetunji@defra.gov.uk>
2023-07-17 07:39:19 +0100

    Added fluent validators for InvoiceHeader and InvoiceLine

---
+++ EST.MIT.Invoice.Api/Models/InvoiceHeaderValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace Invoices.Api.Models;
+
+public class InvoiceHeaderValidator : AbstractValidator<InvoiceHeader>
+{
+    public InvoiceHeaderValidator()
+    {
+        RuleFor(x => x.AgreementNumber).NotEmpty();
+        RuleFor(x => x.AppendixReferences).NotEmpty();
+        RuleFor(x => x.FRN).NotEmpty();
+        RuleFor(x => x.InvoiceLines).NotEmpty();
+        RuleFor(x => x.SourceSystem).NotEmpty();
+        RuleFor(x => x.ContractNumber).NotEmpty();
+        RuleFor(x => x.DeliveryBody).NotEmpty();
+        RuleFor(x => x.DueDate).NotEmpty();
+        RuleFor(x => x.MarketingYear).NotEmpty();
+        RuleFor(x => x.PaymentRequestId).NotEmpty();
+        RuleFor(x => x.PaymentRequestNumber).NotEmpty();
+        RuleFor(x => x.Value).NotEmpty();
+        RuleForEach(x => x.InvoiceLines).SetValidator(new InvoiceLineValidator());
+    }
+}
+
+
---
+++ EST.MIT.Invoice.Api/Models/InvoiceLineValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Invoices.Api.Models;
+
+public class InvoiceLineValidator : AbstractValidator<InvoiceLine>
+{
+    public InvoiceLineValidator()
+    {
+        RuleFor(x => x.SchemeCode).NotEmpty();
+        RuleFor(x => x.Value).NotEmpty();
+        RuleFor(x => x.Currency).NotEmpty();
+        RuleFor(x => x.Description).NotEmpty();
+        RuleFor(x => x.FundCode).NotEmpty();
+    }
+}
+
