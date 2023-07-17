d50167b226d587070626f1ff85ac56b4f042804a
Henry Adetunji <Henry.Adetunji@defra.gov.uk>
2023-07-17 07:46:15 +0100

    Added basic validation rules to InvoiceHeader and InvoiceLine

--- EST.MIT.Invoice.Api/Models/InvoiceLine.cs
+++ EST.MIT.Invoice.Api/Models/InvoiceLine.cs
@@ -1,16 +1,20 @@
 using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace Invoices.Api.Models;
 
 public class InvoiceLine
 {
     [JsonProperty("value")]
+    [Range(0, 999999999999.99, ErrorMessage = "Value must be between 0 and 999999999999.99")]
     public decimal Value { get; set; }
     [JsonProperty("currency")]
     public string Currency { get; set; } = null!;
     [JsonProperty("schemeCode")]
+    [Required(ErrorMessage = "SchemeCode must be specified")]
     public string SchemeCode { get; set; } = null!;
     [JsonProperty("description")]
+    [Required(ErrorMessage = "Description must be stated")]
     public string Description { get; set; } = null!;
     [JsonProperty("fundCode")]
     public string FundCode { get; set; } = null!;
