5d85ea5be713ac9a6558758ef8db3c7183be1285
Henry Adetunji <Henry.Adetunji@defra.gov.uk>
2023-07-28 11:39:30 +0100

    Created Organisation class

---
+++ EST.MIT.Invoice.Api/Services/Api/Models/Organisation.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace EST.MIT.Invoice.Api.Services.Api.Models
+{
+    public class Organisation
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; } = default!;
+        [JsonProperty("description")]
+        public string Description { get; set; } = default!;
+    }
+}
