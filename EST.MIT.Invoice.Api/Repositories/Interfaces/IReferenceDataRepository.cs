818776a23e532d22d6b2c29fb699f26fb112ac81
Henry Adetunji <Henry.Adetunji@defra.gov.uk>
2023-08-18 09:24:20 +0100

    Added the GetSchemeCodesListAsync method to IReferenceDataRepository with its implementaion

--- EST.MIT.Invoice.Api/Repositories/Interfaces/IReferenceDataRepository.cs
+++ EST.MIT.Invoice.Api/Repositories/Interfaces/IReferenceDataRepository.cs
@@ -5,4 +5,5 @@ public interface IReferenceDataRepository
     Task<HttpResponseMessage> GetSchemeTypesListAsync(string? invoiceType, string? organisation);
     Task<HttpResponseMessage> GetPaymentTypesListAsync(string? invoiceType, string? organisation, string? schemeType);
     Task<HttpResponseMessage> GetOrganisationsListAsync(string? invoiceType);
+    Task<HttpResponseMessage> GetSchemeCodesListAsync(string? invoiceType, string? organisation, string? paymentType, string? schemeType);
 }
