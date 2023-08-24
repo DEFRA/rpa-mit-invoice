7cc8c3a19914261d6d2a68b8e25814ccfa1a6406
Henry Adetunji <Henry.Adetunji@defra.gov.uk>
2023-08-24 16:23:40 +0100

    Added  GetFundCodesAsync method to IReferenceDataApi plus its implementations

--- EST.MIT.Invoice.Api/Services/Api/Interfaces/IReferenceDataApi.cs
+++ EST.MIT.Invoice.Api/Services/Api/Interfaces/IReferenceDataApi.cs
@@ -8,4 +8,5 @@ public interface IReferenceDataApi
     Task<ApiResponse<IEnumerable<PaymentType>>> GetPaymentTypesAsync(string? invoiceType, string? organisation, string? schemeType);
     Task<ApiResponse<IEnumerable<Organisation>>> GetOrganisationsAsync(string? invoiceType);
     Task<ApiResponse<IEnumerable<SchemeCode>>> GetSchemeCodesAsync(string? invoiceType, string? organisation, string? paymentType, string? schemeType);
+    Task<ApiResponse<IEnumerable<FundCode>>> GetFundCodesAsync(string? invoiceType, string? organisation, string? paymentType, string? schemeType);
 }
