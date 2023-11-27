9473ed51d1072aec7341ee9482cc6a71c4e56745
fshehzad-defra <faisal.shehzad@defra.gov.uk>
2023-11-27 15:52:13 +0000

    269031, Add in user info to the invoices

---
+++ EST.MIT.Invoice.Api/Services/Api/MockedDataService.cs
@@ -0,0 +1,17 @@
+using EST.MIT.Invoice.Api.Services.Api.Interfaces;
+using EST.MIT.Invoice.Api.Models;
+
+
+namespace EST.MIT.Invoice.Api.Services.Api;
+
+public class MockedDataService : IMockedDataService
+{
+	public LoggedInUser GetLoggedInUser()
+    {
+	    return new LoggedInUser()
+	    {
+		    UserId = "1",
+		    EmailAddress = "mockeduser@defra.gov.uk",
+	    };
+    }
+}
